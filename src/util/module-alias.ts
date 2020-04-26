2cafa26f77fcac0dd7f99eb9ab3cfd68a85e14c3
Waldemar Neto <waldemarnt@gmail.com>
2020-04-26 12:00:20 +1000

    Add module-alias

---
+++ src/util/module-alias.ts
@@ -0,0 +1,8 @@
+import * as path from 'path';
+import moduleAlias from 'module-alias';
+
+const files = path.resolve(__dirname, '../..');
+
+moduleAlias.addAliases({
+  '@': path.join(files, 'src')
+});
