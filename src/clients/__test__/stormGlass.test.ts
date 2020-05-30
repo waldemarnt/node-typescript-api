a1efc2220fa2f9666eb3e12fce8b669ac8ecfdcc
Waldemar Neto <waldemarnt@gmail.com>
2020-05-30 16:44:48 +1000

    Add error handling for StormGlass responses

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -39,7 +39,7 @@ describe('StormGlass client', () => {
     expect(response).toEqual([]);
   });
 
-  it('should get a generic error from Storm Glass service', async () => {
+  it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
@@ -49,6 +49,24 @@ describe('StormGlass client', () => {
 
     await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
       'Unexpected error when trying to communicate to StormGlass: Network Error'
+    );
+  });
+
+  it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { errors: ['Rate Limit reached'] },
+      },
+    });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
     );
   });
 });
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -51,6 +51,14 @@ export class ClientRequestError extends InternalError {
   }
 }
 
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error returned by the StormGlass service';
+    super(`${internalMessage}: ${message}`);
+  }
+}
+
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
@@ -70,6 +78,16 @@ export class StormGlass {
       );
       return this.normalizeResponse(response.data);
     } catch (err) {
+      /**
+       * This is handling the Axios errors specifically
+       */
+      if (err.response && err.response.status) {
+        throw new StormGlassResponseError(
+          `Error: ${JSON.stringify(err.response.data)} Code: ${
+            err.response.status
+          }`
+        );
+      }
       throw new ClientRequestError(err.message);
     }
   }
