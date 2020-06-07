4909ea350fa72c285a3b49d959da5fb2644d2531
Waldemar Neto <waldemarnt@gmail.com>
2020-06-08 09:49:45 +1000

    Add green step to beaches controller

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { Application } from 'express';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
 
 export class SetupServer extends Server {
   /*
@@ -31,7 +32,8 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
   public getApp(): Application {
