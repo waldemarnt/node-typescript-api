61affed0de3d160215af14f8979b09d54bc6b6e1
Waldemar Neto <waldemarnt@gmail.com>
2020-06-08 13:30:55 +1000

    Add the Beach Model

---
+++ src/models/beach.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+const schema = new mongoose.Schema(
+  {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+    name: { type: String, required: true },
+    position: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+export interface Beach {
+  _id?: string;
+  name: string;
+  position: Position;
+  lat: number;
+  lng: number;
+}
+
+type BeachModel = Exclude<Beach, '_id'> & Document;
+
+export const Beach: Model<BeachModel> = mongoose.model('Beach', schema);
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -19,5 +19,24 @@ describe('Beaches functional tests', () => {
       //Object containing matches the keys and values, even if includes other keys such as id.
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
+    it('should throw 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+      });
+    });
+
+    it.skip('should return 500 when there any error other than validation error', async () => {
+      //TODO think in a way to throw a 500
+    });
   });
 });
