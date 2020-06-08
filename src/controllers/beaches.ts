a528aa526afd4138d0b34bba3662e22f50bf622c
Waldemar Neto <waldemarnt@gmail.com>
2020-06-08 13:28:03 +1000

    Add beaches controller and tests in green state

---
+++ src/controllers/beaches.ts
@@ -0,0 +1,10 @@
+import { Controller, Post } from '@overnightjs/core';
+import { Request, Response } from 'express';
+
+@Controller('beaches')
+export class BeachesController {
+  @Post('')
+  public async create(req: Request, res: Response): Promise<void> {
+    res.status(201).send(req.body);
+  }
+}
---
+++ test/functional/beaches.test.ts
@@ -0,0 +1,23 @@
+import { Beach } from '@src/models/beach';
+
+describe('Beaches functional tests', () => {
+  beforeEach(async () => {
+    await Beach.deleteMany({});
+  });
+
+  describe('When creating a new beach', () => {
+    it('should create a beach with success', async () => {
+      const newBeach = {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(201);
+      //Object containing matches the keys and values, even if includes other keys such as id.
+      expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+  });
+});
