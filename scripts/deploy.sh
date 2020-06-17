744af9d17c87b972dbb619639347eed173791b11
Waldemar Neto <waldemarnt@gmail.com>
2020-06-17 15:21:46 +1000

    Add deployment logic

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Init .ssh dir and expand $SSH_KEY
+mkdir -p ~/.ssh
+echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+# Add umbler's git to the list of known_hosts to prevent terminal for asking to confirm the host
+ssh-keyscan -p 9922 -t rsa geonosis.deploy.umbler.com 2>&1 >> ~/.ssh/known_hosts
