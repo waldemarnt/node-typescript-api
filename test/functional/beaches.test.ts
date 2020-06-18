1e3a1b9fcddd0a9b4dfaf4a47b530e48591ddeed
Waldemar Neto <waldemarnt@gmail.com>
2020-06-18 20:25:12 +1000

    Change error message to be swagger

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -52,8 +52,7 @@ describe('Beaches functional tests', () => {
       expect(response.body).toEqual({
         code: 400,
         error: 'Bad Request',
-        message:
-          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+        message: 'request.body.lat should be number',
       });
     });
 
