b2d6bbd6813c110942b9f79aa8dc254f7081543b
Waldemar Neto <waldemarnt@gmail.com>
2020-06-18 16:27:01 +1000

    Integrate rating with the app

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,5 +1,6 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import stormGlassNormalizedResponse1HourMultiplePointsFixture from '@test/fixtures/stormglass_normalized_response_1_hour_multiple_points.json';
 import { Forecast, ForecastProcessingInternalError } from '../forecast';
 import { Beach, BeachPosition } from '@src/models/beach';
 
@@ -7,6 +8,76 @@ jest.mock('@src/clients/stormGlass');
 
 describe('Forecast Service', () => {
   const mockedStormGlassService = new StormGlass() as jest.Mocked<StormGlass>;
+
+  it('should return the forecast for a list of beaches ordered by rating', async () => {
+    mockedStormGlassService.fetchPoints.mockResolvedValue(
+      stormGlassNormalizedResponse1HourMultiplePointsFixture
+    );
+    const beaches: Beach[] = [
+      {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: BeachPosition.E,
+        user: 'fake-id',
+      },
+    ];
+    const expectedResponse = [
+      {
+        time: '2020-04-26T00:00:00+00:00',
+        forecast: [
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 3,
+            swellDirection: 64.26,
+            swellHeight: 0.15,
+            swellPeriod: 13.89,
+            time: '2020-04-26T00:00:00+00:00',
+            waveDirection: 231.38,
+            waveHeight: 2.07,
+            windDirection: 299.45,
+            windSpeed: 100,
+          },
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 2,
+            swellDirection: 123.41,
+            swellHeight: 0.21,
+            swellPeriod: 3.67,
+            time: '2020-04-26T00:00:00+00:00',
+            waveDirection: 232.12,
+            waveHeight: 0.46,
+            windDirection: 310.48,
+            windSpeed: 100,
+          },
+          {
+            lat: -33.792726,
+            lng: 151.289824,
+            name: 'Manly',
+            position: 'E',
+            rating: 2,
+            swellDirection: 182.56,
+            swellHeight: 0.28,
+            swellPeriod: 3.44,
+            time: '2020-04-26T00:00:00+00:00',
+            waveDirection: 232.86,
+            waveHeight: 0.46,
+            windDirection: 321.5,
+            windSpeed: 100,
+          },
+        ],
+      },
+    ];
+    const forecast = new Forecast(mockedStormGlassService);
+    const beachesWithRating = await forecast.processForecastForBeaches(beaches);
+    expect(beachesWithRating).toEqual(expectedResponse);
+  });
   it('should return the forecast for a list of beaches', async () => {
     mockedStormGlassService.fetchPoints.mockResolvedValue(
       stormGlassNormalizedResponseFixture
@@ -29,7 +100,7 @@ describe('Forecast Service', () => {
             lng: 151.289824,
             name: 'Manly',
             position: 'E',
-            rating: 1,
+            rating: 2,
             swellDirection: 64.26,
             swellHeight: 0.15,
             swellPeriod: 3.89,
@@ -49,7 +120,7 @@ describe('Forecast Service', () => {
             lng: 151.289824,
             name: 'Manly',
             position: 'E',
-            rating: 1,
+            rating: 2,
             swellDirection: 123.41,
             swellHeight: 0.21,
             swellPeriod: 3.67,
@@ -69,7 +140,7 @@ describe('Forecast Service', () => {
             lng: 151.289824,
             name: 'Manly',
             position: 'E',
-            rating: 1,
+            rating: 2,
             swellDirection: 182.56,
             swellHeight: 0.28,
             swellPeriod: 3.44,
