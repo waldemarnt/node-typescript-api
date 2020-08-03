5d9dd07b86880728408e66f3a951cf8d0f9a2ae3
Waldemar <waldemarnt@gmail.com>
2020-08-04 06:39:47 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # Add umbler's git to the list of known_hosts to prevent terminal for asking to confirm the host
-ssh-keyscan -p 9922 -t rsa kamino.deploy.umbler.com 2>&1 >> ~/.ssh/known_hosts
+ssh-keyscan -p 9922 -t rsa geonosis.deploy.umbler.com 2>&1 >> ~/.ssh/known_hosts
