66ed66b796eedc7cba0cc767d519d9832bc315e8
Bruno Afonso <bruno.afonso@mutantbr.com>
2020-09-05 16:28:04 -0300

    fix: removing the logic to custom order to another PR

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,9 +1,10 @@
+import _ from 'lodash';
+
 import { StormGlass, ForecastPoint } from '@src/clients/stormGlass';
 import { InternalError } from '@src/util/errors/internal-error';
 import { Beach } from '@src/models/beach';
 import logger from '@src/logger';
 import { Rating } from './rating';
-import orderByCustomProp, { Ordination } from '@src/util/orderByCustomProp';
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {
   rating: number;
@@ -27,29 +28,16 @@ export class Forecast {
   ) {}
 
   public async processForecastForBeaches(
-    beaches: Beach[],
-    {
-      customProp,
-      ordination,
-    }: {
-      customProp: keyof Omit<BeachForecast, '_id'>;
-      ordination: Ordination;
-    } = {
-      customProp: 'rating',
-      ordination: Ordination.DESC,
-    }
+    beaches: Beach[]
   ): Promise<TimeForecast[]> {
     try {
       const beachForecast = await this.calculateRating(beaches);
       const timeForecast = this.mapForecastByTime(beachForecast);
       return timeForecast.map((t) => ({
         time: t.time,
-        forecast: t.forecast.sort((curr, next) =>
-          orderByCustomProp<Omit<BeachForecast, '_id'>>(curr, next, {
-            customProp,
-            ordination,
-          })
-        ),
+        // TODO Allow ordering to be dynamic
+        // Sorts the beaches by its ratings
+        forecast: _.orderBy(t.forecast, ['rating'], ['desc']),
       }));
     } catch (error) {
       throw new ForecastProcessingInternalError(error.message);
