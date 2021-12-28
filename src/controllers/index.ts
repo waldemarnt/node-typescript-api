05294afd8359b1c49b6a72a6e9e46db277f9a0e0
Waldemar <waldemarnt@gmail.com>
2021-12-29 09:56:53 +1000

    Fix merge

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -14,7 +14,7 @@ export abstract class BaseController {
         .status(clientErrors.code)
         .send({ code: clientErrors.code, error: clientErrors.error });
     } else {
-      logger.error(error);
+      logger.error(JSON.stringify(error));
       res.status(500).send({ code: 500, error: 'Something went wrong!' });
     }
   }
--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,10 @@ import { connect as mongooseConnect, connection } from 'mongoose';
 const dbConfig: IConfig = config.get('App.database');
 
 export const connect = async (): Promise<void> => {
-  await mongooseConnect(dbConfig.get('mongoUrl'));
+  await mongooseConnect(dbConfig.get('mongoUrl'), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 };
 
 export const close = (): Promise<void> => connection.close();
