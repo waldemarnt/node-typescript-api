49900965934e4276dd931f62540eb629358a1a0f
José Lazaretti <eduardo1793@gmail.com>
2022-07-28 14:52:22 -0300

    Adding testcontainers

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,11 +1,31 @@
 import { SetupServer } from '@src/server';
 import supertest from 'supertest';
+import path from 'path';
+import {
+  DockerComposeEnvironment,
+  StartedDockerComposeEnvironment,
+  Wait,
+} from 'testcontainers';
+
+const composeFilePath = path.resolve(__dirname, '..');
+const composeFile = 'docker-compose.yml';
+let environment: StartedDockerComposeEnvironment;
 
 let server: SetupServer;
+
+jest.setTimeout(20000);
+
 beforeAll(async () => {
+  environment = await new DockerComposeEnvironment(composeFilePath, composeFile)
+    .withWaitStrategy('mongodb', Wait.forHealthCheck())
+    .up(['mongodb']);
+
   server = new SetupServer();
   await server.init();
   global.testRequest = supertest(server.getApp());
 });
 
-afterAll(async () => await server.close());
+afterAll(async () => {
+  await server.close();
+  await environment.down();
+});
